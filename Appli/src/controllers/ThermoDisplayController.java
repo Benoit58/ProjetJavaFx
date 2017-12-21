ade48c8a3499e4ee14df90a9137cfd7c24ec7b43
Benoit58 <33517805+Benoit58@users.noreply.github.com>
2017-12-21 01:14:14 +0100

    Modification de l'affichage Thermometre et de son controlleur

--- Appli/src/controllers/ThermoDisplayController.java
+++ Appli/src/controllers/ThermoDisplayController.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+import metier.Sensor;
+
 public class ThermoDisplayController {
+    @FXML
+    Label maxLabel;
+
+    @FXML
+    Label tempLabel;
+
+    @FXML
+    Label minLabel;
+
+    @FXML
+    Rectangle rectangleLabel;
+
+    @FXML
+    Label sensorName;
+
+
+    public void load(Sensor sensor){
+        sensorName.textProperty().bind(sensor.sensorNameProperty());
+        tempLabel.textProperty().bind((sensor.temperatureProperty().asString()));
+        rectangleLabel.heightProperty().bind(sensor.temperatureProperty().multiply(90).divide(200).add(45));
+    }
 }
