52d6965178191382fc6cf6692cabf4f338570ed4
Benoit58 <33517805+Benoit58@users.noreply.github.com>
2017-12-21 01:13:49 +0100

    Modification de l'affichage Icon et de son controlleur

--- Appli/src/controllers/IconDIsplayController.java
+++ Appli/src/controllers/IconDIsplayController.java
@@ -1,4 +1,31 @@
 package controllers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import metier.Sensor;
+
+
 public class IconDIsplayController {
+    Image sun=new Image("/img/sun.png");
+    Image cloud=new Image("/img/cloud.png");
+    Image snowflake=new Image("/img/snowflake.png");
+
+    @FXML
+    Label sensorName;
+
+    @FXML
+    ImageView icon;
+
+    public void load(Sensor sensor){
+        sensorName.textProperty().bind(sensor.sensorNameProperty());
+        sensor.temperatureProperty().addListener((o,oldV,newV)->{
+            if ((int) newV < 0) icon.setImage(snowflake);
+            if ((int) newV > 20) icon.setImage(sun);
+            if ((int) newV >= 0 && (int) newV <= 20) icon.setImage(cloud);
+        });
+    }
 }
