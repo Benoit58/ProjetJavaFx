0d0534bb69b11912e49af0ad0c59d57e24c7bd8c
Benoit58 <33517805+Benoit58@users.noreply.github.com>
2017-12-21 01:12:03 +0100

    Ajout des icones

--- Appli/src/model/SensorModel.java
+++ Appli/src/model/SensorModel.java
@@ -8,16 +8,19 @@ import javafx.collections.ObservableList;
 
 public class SensorModel {
 
-    private ObservableList<Sensor>  sensorList = FXCollections.observableArrayList();
+    private ObservableList<Sensor> sensorListObs = FXCollections.observableArrayList();
 
-    private final ListProperty<Sensor> sensors = new SimpleListProperty<>(sensorList);
+    private final ListProperty<Sensor> sensors = new SimpleListProperty<>(sensorListObs);
         public ObservableList<Sensor> getSensors() { return sensors.get(); }
+        public void setSensors(ObservableList<Sensor> value){sensors.set(value);}
         public ListProperty<Sensor> sensorProperty(){return sensors; }
 
-    public SensorModel(){
-        sensorList.add(new Sensor("Bureau", 20, 1));
-        sensorList.add(new Sensor("Salon", 25, 4));
-        sensorList.add(new Sensor("Cuisine", 28, 12));
+    public  SensorModel(){
+        sensorListObs.add(new Sensor("Bureau", 20, 1));
+        sensorListObs.add(new Sensor("Salon", 25, 4));
+        sensorListObs.add(new Sensor("Cuisine", 28, 12));
+        sensorListObs.add(new Sensor("0°C", 0, 1));
+        sensorListObs.add(new Sensor("100°C", 100, 1));
     }
 
 }
