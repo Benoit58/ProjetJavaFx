66cff7a4c48c765ff3ec785a8e247e0d9f20a4cc
Yannis Mahiou <yannis.mahiou1@gmail.com>
2018-01-15 18:10:37 +0100

    ww

--- Appli/src/business_logic/visitor/SensorVisitor.java
+++ Appli/src/business_logic/visitor/SensorVisitor.java
@@ -2,7 +2,12 @@ package business_logic.visitor;
 
 import business_logic.sensor.ISensor;
 
-public class SensorVisitor implements Visitor{
-    public void visit(ISensor sensor);
-
+public class SensorVisitor extends Visitor{
+    public void visit(Object sensor) {
+        try {
+            getMethod(sensor.getClass()).invoke(this, o);
+        } catch (Exception ex) {
+            System.out.println( "DownVisitor - no appropriate visit() method" );
+        }
+    }
 }
--- Appli/src/business_logic/visitor/Visitor.java
+++ Appli/src/business_logic/visitor/Visitor.java
@@ -2,7 +2,44 @@ package business_logic.visitor;
 
 import business_logic.sensor.ISensor;
 
-public interface Visitor {
-    public void visit(ISensor sensor);
-    
+import java.lang.reflect.Method;
+
+public abstract class Visitor {
+    abstract void visit(Object sensor);
+
+    protected Method getMethod(Class source){
+        Class  clazz = source;
+        Method methodName   = null;
+        while (methodName == null  &&  clazz != Object.class) {
+            String clazzName = clazz.getName();
+            clazzName = "visit" + clazzName.substring(clazzName.lastIndexOf('.') + 1);
+            try {
+                methodName = getClass().getMethod(clazzName, clazz);
+            } catch (NoSuchMethodException ex) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        if (clazz == Object.class) {
+            System.out.println( "Searching for interfaces" );
+            Class[] interfaces = source.getInterfaces();
+            for (Class intface : interfaces) {
+                String interfaceName = intface.getName();
+                interfaceName = "visit" + interfaceName.substring(interfaceName.lastIndexOf('.') + 1);
+                try {
+                    methodName = getClass().getMethod(interfaceName, intface);
+                } catch (NoSuchMethodException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        if (methodName == null)
+            try {
+                methodName = getClass().getMethod("visitObject", Object.class);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        return methodName;
+    }
+    }
+
 }
